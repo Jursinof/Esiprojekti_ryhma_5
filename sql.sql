e9dce26f3c017b4cb73de8fcb13a2c94cceafeeb
saarakoskinen <saaraidk@metropolia.com>
2025-03-06 13:57:44 +0200

    kysymyksiä

---
+++ sql.sql
@@ -0,0 +1,43 @@
+INSERT INTO questions (maa, kysymys, vaihtoehdot, oikea_vastaus) VALUES
+('Saksa', 'Mikä on Saksan suurin kaupunki väkiluvultaan?', '{"A": "München", "B": "Berliini", "C": "Frankfurt"}', 'B'),
+('Saksa', 'Mikä vuosi oli Saksan yhdistymisvuosi?', '{"A": "1989", "B": "1990", "C": "2000"}', 'B'),
+('Saksa', 'Mikä on Saksan pisin joki?', '{"A": "Rein", "B": "Elbe", "C": "Tonava"}', 'A'),
+('Saksa', 'Mitkä ovat Saksan lipun värit järjestyksessä ylhäältä alas?', '{"A": "Musta, punainen, keltainen", "B": "Punainen, musta, keltainen", "C"): "Musta, keltainen, punainen"}', 'A'),
+('Saksa', 'Minkä tunnetun saksalaisen säveltäjän teos on "Oodi ilolle"?', '{"A": "Johann Sebastian Bach", "B": "Richard Wagner", "C": "Ludwig van Beethoven"}', 'C'),
+('Saksa', 'Mikä on Saksan tunnetuin festivaali, joka pidetään Münchenissä?', '{"A": "Berlinale", "B": "Carnival of Cultures", "C": "Oktoberfest"}', 'C'),
+
+('Italia', 'Mikä on Italian asutuin kaupunki?', '{"A": "Rooma", "B": "Milano", "C": "Napoli"}', 'A'),
+('Italia', 'Kuka on Italian nykyinen presidentti (2025)?', '{"A": "Giorgio Meloni", "B": "Sergio Mattarella", "C": "Giorgio Napolitano"}', 'B'),
+('Italia', 'Mikä on Italian pinta-ala (km^2)?', '{"A": "302 073", "B": "100 557", "C": "204 077"}', 'A'),
+('Italia', 'Milloin on Italian itsenäisyyspäivä?', '{"A": "20.10", "B": "17.3", "C": "13.6"}', 'B'),
+('Italia', 'Mikä näistä EI ole Italian naapurivaltio?', '{"A": "Ranska", "B": "Sveitsi", "C": "Slovakia"}', 'C'),
+('Italia', '', '{"A": "Rooma", "B": "Venetsia", "C": "Firenze"}', 'C'),
+
+('Irlanti', 'Mikä on Irlannin kansallissymboli?', '{"A": "Apila", "B": "Ruusu", "C": "Ohdake"}', 'A'),
+('Irlanti', 'Mikä on Irlannin virallinen valuutta?', '{"A": "Englannin punta", "B": "Euro", "C": "Dollari"}', 'B'),
+('Irlanti', 'Mikä juhlapäivä on saanut alkunsa Irlannista', '{"A": "Halloween", "B": "Joulu", "C": "Pääsiäinen"}', 'A'),
+('Irlanti', 'Mikä on Irlannin lempinimi', '{"A": "Kultamaa", "B": "Smaragdisaari", "C": "Safiirimaa"}', 'B'),
+('Irlanti', 'Minkä valtameren rannalla Irlanti sijaitsee?', '{"A": "Tyyni valtameri", "B": "Jäämeri", "C": "Atlantin valtameri"}', 'C'),
+('Irlanti', 'Mikä on tunnettu perinteinen Irlantilainen soitin?', '{"A": "Viulu", "B": "Harmonikka", "C": "Harppu"}', 'C'),
+
+('Kreikka', 'Mikä on Kreikan pääuskonto?', '{"A": "Katolisuus", "B": "Ortodoksinen kristinusko", "C": "Islam"}', 'B'),
+('Kreikka', 'Mikä meri ympäröi suurimman osan Kreikkaa?', '{"A": "Välimeri", "B": "Punainen meri", "C": "Mustameri"}', 'A'),
+('Kreikka', 'Mikä tunnettu rakennus sijaitsee Kreikassa Akropoliksella?', '{"A": "Colosseum", "B": "Partheneon", "C": "Riemukaari"}', 'B'),
+('Kreikka', 'Mikä kreikkalaisen mytologian jumala hallitsi Olymposta?', '{"A": "Poseidon", "B": "Hades", "C": "Zeus"}', 'C'),
+('Kreikka', 'Mikä on tyypillinen Kreikkalainen ruokalaji?', '{"A": "Moussaka", "B": "Sushi", "C": "Paella"}', 'A'),
+('Kreikka', 'Mikä on Kreikan virallinen valuutta?', '{"A": "Drachma", "B": "Punta", "C": "Euro"}', 'C'),
+
+('Tanska', 'Mikä Tanskalainen kirjailija on tunnetuin maailmalla?', '{"A": "H. C. Andersen", "B": "Soren Kierkegrd", "C": "Niels Bohr"}', 'A'),
+('Tanska', 'Mikä on Tanskan kansallisruoka?', '{"A": "Smörrebröd", "B": "Frikadeller", "C": "Stegtfläsk"}', 'C'),
+('Tanska', 'Mikä on Tanskan suurin saari?', '{"A": "Själland", "B": "Fyn", "C": "Bornholm"}', 'A'),
+('Tanska', 'Kuka on Tanskan nykyinen kuningas (2025)?', '{"A": "Frederik V", "B": "Frederik XI", "C": "Frederik X"}', 'C'),
+('Tanska', 'Onko Kööpenhamina Tanskan asutuin kaupunki?', '{"Ei": "", "B": "Kyllä", "C": "Sillä on sama väkiluku, kuin Aarhusilla"}', 'B'),
+('Tanska', 'Mikä on Tanskan valtiomuoto?', '{"A": "Demokraattinen tasavalta", "B": "Parlamentaarinen perustuslaillinen monarkia", "C": "Parlamentaarinen perustuslaillinen tasavalta"}', 'B'),
+
+('Ruotsi', 'Mikä näistä EI ole Ruotsin vähemmistökieli?', '{"A": "Suomi", "B": "Romani", "C": "Saksa"}', 'C'),
+('Ruotsi', 'Mikä on Ruotsin pinta-ala (km^2)?', '{"A": "450 295", "B": "300 566", "C": "841 786"}', 'A'),
+('Ruotsi', 'Mikä on ruotsin kuninkaallinen vaalilause?', '{"A": "Ruotsi eteenpäin ruhtinaallisesti", "B": "Ruotsi eteenpäin prinsessan nimeen", "C": "Ruotsin hyväksi ajan hengessä"}', 'C'),
+('Ruotsi', 'Kuka on Ruotsin nykyinen kuningas (2025)?', '{"A": "Ulf Kristensson V", "B": "Kaarle XVI Kustaa", "C": "Kaarle X Kustaa"}', 'B'),
+('Ruotsi', 'Mikä on Ruotsin valtiomuoto?', '{"A": "Perustuslaillinen monarkia", "B": "Demokraattinen monarkia", "C": "Parlamentaarinen perustuslaillinen monarkia"}', 'A'),
+('Ruotsi', 'Mikä näistä on Ruotsalainen yritys?', '{"A": "Prada", "B": "Skanska", "C": "Cubus"}', 'B')
+
